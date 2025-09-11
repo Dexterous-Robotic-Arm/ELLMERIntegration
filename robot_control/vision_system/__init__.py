0ef72a03a4bc49a8fda18337ad72a2bd5846a781
Vorobiy <eugenevorob@gmail.com>
2025-09-11 14:38:55 -0400

    cleanup

--- robot_control/vision_system/__init__.py
+++ robot_control/vision_system/__init__.py
@@ -6,6 +6,5 @@ and pose estimation using April Tags and RealSense D435.
 """
 
 from .pose_recorder import PoseRecorder
-from .april_tag_detector import AprilTagDetector
 
-__all__ = ['PoseRecorder', 'AprilTagDetector'] 
+__all__ = ['PoseRecorder'] 
